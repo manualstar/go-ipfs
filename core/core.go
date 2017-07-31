bd7392728f7837f5da18b8de4bc621d96b0f8844
vyzo <vyzo@hackzen.org>
2017-07-31 21:51:20 +0300

    fix competing address factories in host options
    integrate #3948
    
    License: MIT
    Signed-off-by: vyzo <vyzo@hackzen.org>

--- core/core.go
+++ core/core.go
@@ -793,9 +793,6 @@ func constructPeerHost(ctx context.Context, id peer.ID, ps pstore.Peerstore, bwr
 	if !opts.DisableNatPortMap {
 		hostOpts = append(hostOpts, p2pbhost.NATPortMap)
 	}
-	if opts.AddrsFactory != nil {
-		hostOpts = append(hostOpts, opts.AddrsFactory)
-	}
 
 	addrsFactory := opts.AddrsFactory
 	if !opts.DisableRelay {
